19e5476816b986e4fdc931c7dbc74cf2dc2e078a
lsh <lsh6746@aininetworks.com>
2024-09-26 09:33:15 +0900

    deploy test

--- src/main/java/com/book/aladin/service/AladinService.java
+++ src/main/java/com/book/aladin/service/AladinService.java
@@ -34,7 +34,7 @@ public class AladinService {
                 .querytype(bookFilterDto.getQueryType())
                 .start(bookFilterDto.getStartIdx())
                 .maxResults(bookFilterDto.getMaxResults()).build();
-        log.info("test1");
+        log.info("test2");
         return Optional.ofNullable(this.getApi(ITEM_LIST, apiParam).getItem());
     }
 
