3155f90157b3d89a5dcdeacaa2e37bf5ff745b84
lsh <hack4569@naver.com>
2025-02-18 04:49:00 +0900

    서킷브레이커, spock 적용

--- src/main/java/com/book/aladin/exception/AladinException.java
+++ src/main/java/com/book/aladin/exception/AladinException.java
@@ -1,21 +1,35 @@
 package com.book.aladin.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class AladinException extends RuntimeException{
     private static final String DEFAULT_MESSAGE = "[알라딘 API] 연동 오류 발생";
+    private final String errorMessage;
+    private final HttpStatus httpStatus;
+
+    // 기본 생성자
     public AladinException() {
-        super(DEFAULT_MESSAGE);
+        this(DEFAULT_MESSAGE, HttpStatus.INTERNAL_SERVER_ERROR);  // 기본 httpStatus 설정
     }
 
+    // 메시지만 받아서 생성
     public AladinException(String message) {
-        super(DEFAULT_MESSAGE + " (" + message + ") ");
+        this(DEFAULT_MESSAGE + " (" + message + ")", HttpStatus.INTERNAL_SERVER_ERROR); // 기본 httpStatus 설정
     }
 
+    // 메시지와 원인 받아서 생성
     public AladinException(String message, Throwable cause) {
-        super(DEFAULT_MESSAGE + " (" + message + ") ", cause);
+        super(DEFAULT_MESSAGE + " (" + message + ")", cause);
+        this.errorMessage = DEFAULT_MESSAGE + " (" + message + ") ";
+        this.httpStatus = HttpStatus.INTERNAL_SERVER_ERROR; // 기본 httpStatus 설정
     }
 
-    public AladinException(Throwable cause) {
-        super(DEFAULT_MESSAGE, cause);
+    // 메시지와 HttpStatus 받아서 생성
+    public AladinException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.errorMessage = DEFAULT_MESSAGE + " (" + message + ") ";
+        this.httpStatus = httpStatus; // 받은 httpStatus 설정
     }
-
 }
--- src/main/java/com/book/aladin/service/AladinService.java
+++ src/main/java/com/book/aladin/service/AladinService.java
@@ -3,15 +3,21 @@ package com.book.aladin.service;
 import com.book.aladin.constants.AladinConstants;
 import com.book.aladin.domain.AladinBook;
 import com.book.aladin.domain.AladinMaster;
+import com.book.aladin.exception.AladinException;
 import com.book.common.ApiParam;
 import com.book.recommend.dto.BookFilterDto;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -30,6 +36,7 @@ public class AladinService {
      * @param bookFilterDto
      * @return
      */
+    @CircuitBreaker(name = "aladinSearch", fallbackMethod = "searchFallBack")
     public Optional<List<AladinBook>> bookList(BookFilterDto bookFilterDto) {
         ApiParam apiParam = ApiParam.builder()
                 .querytype(bookFilterDto.getQueryType())
@@ -37,9 +44,29 @@ public class AladinService {
                 .ttbkey(ttbkey)
                 .maxResults(bookFilterDto.getMaxResults()).build();
         var aladinBooks = Optional.ofNullable(this.getApi(AladinConstants.ITEM_LIST, apiParam).getItem());
+        if (aladinBooks.isEmpty()) {
+            throw new AladinException("상품조회시 데이터가 없습니다.");
+        }
         return aladinBooks;
     }
 
+    public Optional<List<AladinBook>> searchFallBack(BookFilterDto bookFilterDto, Throwable throwable) {
+        // 서킷이 open상태가 되었을때
+        if (throwable instanceof CallNotPermittedException) {
+            return handleOpenCircuit(bookFilterDto);
+        }
+        return handleException(bookFilterDto, throwable);
+    }
+
+    private Optional<List<AladinBook>> handleOpenCircuit(BookFilterDto bookFilterDto) {
+        log.warn("Circuit Breaker is open! Fallback to kakao search");
+        return null;
+    }
+
+    private Optional<List<AladinBook>> handleException(BookFilterDto bookFilterDto, Throwable throwable) {
+        log.error( "error occurred! Fallback to kakao search");
+        return null;
+    }
     /**
      * 상품 상세 조회
      * @param apiParam
@@ -51,16 +78,23 @@ public class AladinService {
     }
 
     private AladinMaster getApi(String path, ApiParam apiParam) {
-        ResponseEntity<AladinMaster> response = aladinApi
-                .get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(path)
-                        .queryParams(apiParam.getApiParamMap())
-                        .build()
-                )
-                .retrieve()
-                .toEntity(AladinMaster.class)
-                .block();
-        return response.getBody();
+        ResponseEntity<AladinMaster> response = null;
+        try{
+            response = aladinApi
+                    .get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path(path)
+                            .queryParams(apiParam.getApiParamMap())
+                            .build()
+                    )
+                    .retrieve()
+                    .toEntity(AladinMaster.class)
+                    .block();
+            return response.getBody();
+        } catch (Exception e) {
+            log.error("[알라딘] 에러 메세지 파싱 에러 code={}, errorMessage={}", response.getStatusCodeValue(), e.getMessage(), e);
+            throw new AladinException("파싱에러", HttpStatus.valueOf(response.getStatusCodeValue()));
+        }
+
     }
 }
